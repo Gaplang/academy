fac43b5565415c6fc88076edf54acc6d7c42c9a0
pirha <pirha@DESKTOP-KVV0U5J>
2023-05-25 12:05:23 +0300

    second commit

---
+++ academy/src/main/java/by/academy/lesson3/DoubleDemo.java
@@ -0,0 +1,21 @@
+package by.academy.lesson3;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 30;
+		int b = 5;
+		double d = 123.456 % 5;
+		
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(d);
+
+
+		
+		
+
+	}
+
+}
