0d9a6610043e2e7f7669caeaeac7dcf9ca4dfafc
Gaplang <134556515+Gaplang@users.noreply.github.com>
2023-06-28 02:44:21 +0300

    Update Task4.java

--- academy/src/main/java/by/academy/homework3/Task4.java
+++ academy/src/main/java/by/academy/homework3/Task4.java
@@ -1,28 +1,41 @@
 package by.academy.homework3;
 
-import java.util.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Task4 {
+class Task4 {
 
-	public void parseAndPrintDate(String inputDate) {
-		SimpleDateFormat dateFormat;
-		if (inputDate.contains("/")) {
-			dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		} else {
-			dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	public static final Pattern DATE_FORMAT_DD_MM_YYYY_1 = Pattern
+			.compile("(0?[1-9]|[12][0-9]|3[01])(-)(0?[1-9]|1[012])(-)((19|20)\\d\\d)");
+	public static final Pattern DATE_FORMAT_DD_MM_YYYY_2 = Pattern
+			.compile("(0?[1-9]|[12][0-9]|3[01])(/)(0?[1-9]|1[012])(/)((19|20)\\d\\d)");
+
+	private static final DateTimeFormatter FORMATTER_1 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+	private static final DateTimeFormatter FORMATTER_2 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	public static boolean isValidDate(String p) {
+		return isValidDate1(p) || isValidDate2(p);
+	}
+
+	private static boolean isValidDate1(String p) {
+		Matcher matcher1 = DATE_FORMAT_DD_MM_YYYY_1.matcher(p);
+		return matcher1.matches();
+	}
+
+	private static boolean isValidDate2(String p) {
+		Matcher matcher2 = DATE_FORMAT_DD_MM_YYYY_2.matcher(p);
+		return matcher2.matches();
+	}
+
+	public static LocalDate parse(String date) {
+		if (isValidDate1(date)) {
+			return LocalDate.parse(date, FORMATTER_1);
 		}
-		try {
-			Date date = dateFormat.parse(inputDate);
-			SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy");
-			System.out.println("Day: " + outputFormat.format(date));
-			outputFormat.applyPattern("MM");
-			System.out.println("Month: " + outputFormat.format(date));
-			outputFormat.applyPattern("yyyy");
-			System.out.println("Year: " + outputFormat.format(date));
-		} catch (ParseException e) {
-			System.out.println("Error parsing date.");
+		if (isValidDate2(date)) {
+			return LocalDate.parse(date, FORMATTER_2);
 		}
+		return null;
 	}
 }
